e0e05037b28f4e363a4def1d3c8f7de2a30c2373
Caglar Kilcioglu <ckilcioglu@gmail.com>
2020-08-06 12:13:16 +0300

    Timezone support was added via 'moment' package. This package uses moment.js to control ODM

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -18,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 if not app.debug:
