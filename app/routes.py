27e1c29d379112202fbb7614c4101ccee518bb9a
Caglar Kilcioglu <ckilcioglu@gmail.com>
2020-08-07 22:24:50 +0300

    Localization of datetime data

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm, EmptyForm
 from app.models import User, Post
@@ -6,7 +6,7 @@ from app.email import send_password_reset_email
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
-from flask_babel import _
+from flask_babel import _, get_locale
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -181,3 +181,4 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
