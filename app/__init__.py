b7c39a241b5fdc368227bc441d12e5ddb7c5e8a4
Caglar Kilcioglu <ckilcioglu@gmail.com>
2020-08-14 21:55:13 +0300

    Switched to blueprint model by grouping according to functionalities

--- app/__init__.py
+++ app/__init__.py
@@ -17,12 +17,21 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-login.login_view = 'login'
+login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
+
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
 
 if not app.debug:
     # Handle email notification for critical errors
@@ -59,4 +68,4 @@ if not app.debug:
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-from app import routes, models, errors, email
+from app import models
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, Email, ValidationError, Length
-from app.models import User
 from flask_babel import _, lazy_gettext as _l
+from app.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField(_l("Username"), validators=[DataRequired()])
     password = PasswordField(_l("Password"), validators=[DataRequired()])
     remember_me = BooleanField(_l("Remember Me"))
     submit = SubmitField(_l("Sign In"))
-
 
 class RegistrationForm(FlaskForm):
     username = StringField(_l("Username"), validators=[DataRequired()])
